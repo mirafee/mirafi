97bd34aaecae3087e038d76ea4cbe13c3aa828a5
Mominul Islam Rafi <93321460+mirafee@users.noreply.github.com>
2021-11-10 18:48:41 +0600

    Update types.ts

--- types.ts
+++ types.ts
@@ -16,8 +16,6 @@ export interface IProject {
   name: string;
   description: string;
   image_path: string;
-  deployed_url: string;
-  github_url: string;
   category: Category[];
   key_techs: string[];
 }
